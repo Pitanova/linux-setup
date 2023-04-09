38d9a03931ed8d7c9d0d1c846c6fccf58c3db9df
TimSolokha <timsolokha@gmail.com>
2023-04-08 23:08:53 -0400

    First version of script with minor bug

--- setup.sh
+++ setup.sh
@@ -17,4 +17,38 @@ sleep 1
 echo "What are you downloading?"
 echo "A) Terminal Apps"
 echo "B) Flatpak Apps"
-echo "Gaming Apps"
+echo "C) Gaming Apps"
+echo -n "[A/B/C]: "
+read choice
+
+LIST_OF_TERMAPPS="btop \
+	          	  neofetch \
+		  		  git \
+		  		  gimp \
+		  		  tmux"
+
+LIST_OF_FLATPAKS="com.bitwarden.desktop \
+	          	  com.usebottles.bottles \
+		  		  com.discordapp.Discord \
+		  		  com.mattjakeman.ExtensionManager \
+		  		  org.flameshot.Flameshot \
+		  		  com.github.tchx84.Flatseal \
+		  		  org.localsend.localsend_app \
+		  		  org.telegram.desktop \
+		    	  org.videolan.VLC \
+		  		  com.visualstudio.code"
+
+flatpak_manager=$(flatpak install flathub -y --noninteractive)
+
+case $choice in
+    A|a)
+    ${package_manager} $LIST_OF_TERMAPPS
+    ;;
+    B|b)
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    ${flatpak_manager} $LIST_OF_FLATPAKS
+    ;;
+    C|c)
+    echo "This is still being worked on..."
+    exit 1
+esac
