23da2558e965bc5ab210dfe9950dccc634bfc671
TimSolokha <timsolokha@gmail.com>
2023-04-08 23:53:21 -0400

    Added more programs

--- setup.sh
+++ setup.sh
@@ -14,39 +14,57 @@ do
 done
 
 echo "NOTE: Download flatpak and Nerd Fonts before using this script!"
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sleep 1
 
-echo "What are you downloading?"
-echo "A) Terminal Apps"
-echo "B) Flatpak Apps"
-echo "C) Gaming Apps"
-echo -n ": "
+#TODO: Download all relevant stuff to the machine
+
+#TODO: Create a division for extra desktop programs 
+
+echo "How would you like to set up your system?"
+echo "A) Default Apps"
+echo "B) Extra Desktop Apps"
+echo "C) Exit"
+echo -n "[A/B/C]: "
 read choice
 
-LIST_OF_TERMAPPS="btop \
+LIST_OF_DEF_TERMAPPS="btop \
 	          	  neofetch \
 		  		  git \
-		  		  gimp \
 		  		  tmux"
 
-LIST_OF_FLATPAKS="com.bitwarden.desktop \
+LIST_OF_DEF_FLATPAKS="com.bitwarden.desktop \
 	          	  com.usebottles.bottles \
 		  		  com.discordapp.Discord \
 		  		  com.mattjakeman.ExtensionManager \
 		  		  org.flameshot.Flameshot \
 		  		  com.github.tchx84.Flatseal \
+				  com.heroicgameslauncher.hgl \
 		  		  org.localsend.localsend_app \
+				  com.valvesoftware.Steam \
 		  		  org.telegram.desktop \
 		    	  org.videolan.VLC \
 		  		  com.visualstudio.code"
 
+LIST_OF_EXTRA_TERMAPPS="piper \
+						virt-manager"
+
+LIST_OF_EXTRA_FLATPAKS="net.davidotek.pupgui2 \
+					com.leinardi.gwe \
+					org.openrgb.OpenRGB \
+					com.brave.Browser \
+					com.dec05eba.gpu_screen_recorder \
+					org.kde.kdenlive"
+
 case $choice in
     A|a)
-    ${package_manager} $LIST_OF_TERMAPPS
+    ${package_manager} $LIST_OF_DEF_TERMAPPS
+	flatpak install flathub -y --noninteractive $LIST_OF_DEF_FLATPAKS
     ;;
     B|b)
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-	flatpak install flathub -y --noninteractive $LIST_OF_FLATPAKS
+	${package_manager} $LIST_OF_EXTRA_TERMAPPS
+	flatpak install flathub -y --noninteractive $LIST_OF_EXTRA_FLATPAKS
     ;;
     C|c)
     echo "This is still being worked on..."
